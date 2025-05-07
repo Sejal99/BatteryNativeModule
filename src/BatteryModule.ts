d491a7e70c06616035a1c81e8db4828f082a6c0e
sejal0699 <sejal8974@gmail.com>
2025-05-07 12:41:21 +0530

    swift code implemented

--- src/BatteryModule.ts
+++ src/BatteryModule.ts
@@ -1,5 +1,12 @@
-import { NativeModules } from 'react-native';
+import {NativeModules} from 'react-native';
 
-const { BatteryModule } = NativeModules;
+const {BatteryModule} = NativeModules;
 
+BatteryModule.getBatteryLevel()
+  .then(level => {
+    console.log('Battery Level (iOS):', level + '%');
+  })
+  .catch(err => {
+    console.error('Failed to get battery level:', err);
+  });
 export default BatteryModule;
